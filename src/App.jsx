7866c4e46cf73ca0611e29612703bae2b6486fa0
sittipong03 <sittipong.f@gmail.com>
2025-05-16 09:21:09 +0700

    Tailwind daisy template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-3xl border-red-500'>test</h1>
+    <button className='btn'> aaaaaa </button>
+    </>
+  )
+}
+
+export default App
